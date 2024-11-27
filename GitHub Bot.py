39c5e67f91fae837c9a780a56ba9626dc1290a94
Sohmteee <ukaegbesomtochukwu@gmail.com>
2024-11-27 16:17:25 +0100

    commit

--- GitHub Bot.py
+++ GitHub Bot.py
@@ -1,4 +1,3 @@
-
 import os
 import subprocess
 import time
@@ -9,41 +8,40 @@ def run_command(command):
     if result.stderr:
         print(result.stderr)
 
+def setup_git():
+    print("It looks like you haven't added this project to your GitHub yet")
+    print("Let's quickly set it up!\n")
+
+    run_command("git init")
+    run_command("git add .")
+    print("\nAdding commit...")
+    run_command('git commit -m "commit"')
+    print("Added commit successfully")
+    run_command("git branch -M main")
+
+    print("\nCopy and Paste the link to your GitHub repository")
+    print('It should look something like "https://github.com/<your username>/<name of repository>.git"')
+    rep = input("Paste the link here: ")
+    run_command(f"git remote add origin {rep}")
+    print("\nPushing git...")
+    run_command("git push -u origin main")
+
+def update_git():
+    print("\nChecking git status...")
+    run_command("git status")
+    run_command("git add .")
+    print("\nAdding commits...")
+    run_command('git commit -m "commit"')
+    print("Added commits successfully")
+    print("\nPushing git...")
+    run_command("git push")
+
 def main():
     while True:
         if os.path.exists('.git'):
-            print("\nChecking git status...")
-            run_command("git status")
-
-            run_command("git add .")
-
-            print("\nAdding commits...")
-            run_command('git commit -m "commit"')
-            print("Added commits successfully")
-
-            print("\nPushing git...")
-            run_command("git push")
+            update_git()
         else:
-            print("It looks like you haven't added this project to your GitHub yet")
-            print("Let's quickly set it up!\n")
-
-            run_command("git init")
-
-            run_command("git add .")
-
-            print("\nAdding commit...")
-            run_command('git commit -m "commit"')
-            print("Added commit successfully")
-
-            run_command("git branch -M main")
-
-            print("\nCopy and Paste the link to your GitHub repository")
-            print('It should look something like "https://github.com/<your username>/<name of repository>.git"')
-            rep = input("Paste the link here: ")
-            run_command(f"git remote add origin {rep}")
-
-            print("\nPushing git...")
-            run_command("git push -u origin main")
+            setup_git()
 
         print("\nWaiting for 60 seconds...")
         time.sleep(60)
